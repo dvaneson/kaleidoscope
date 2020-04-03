ebe5ab68f91a49b954c2a8665a5717ee6d1b47f9
Donovan Ellison <dvaneson@gmail.com>
2020-04-02 17:10:20 -0700

    Replacing Emit with Emit2 and removing Codegen

--- src/Emit.hs
+++ src/Emit.hs
@@ -1,102 +1,165 @@
+-- An update to the Emit module that uses the LLVM IR builder API instead of Codegen
 {-# LANGUAGE OverloadedStrings #-}
 
-module Emit (codegen, cgen, codegenTop) where
+module Emit (codegen) where
 
-import LLVM.Prelude
-import LLVM.Module
-import LLVM.Context
-
-import qualified LLVM.AST as AST
-import qualified LLVM.AST.Constant as C
-import qualified LLVM.AST.Float as F
-import qualified LLVM.AST.FloatingPointPredicate as FP
-
-import Data.Word
-import Data.Int
 import Data.String
-import Control.Monad.Except
-import Control.Applicative
 import qualified Data.Map as Map
 import qualified Data.ByteString.Char8 as B
 
-import Codegen
+import Control.Monad.State
+
+import LLVM.IRBuilder
+import LLVM.IRBuilder.Module
+import LLVM.IRBuilder.Internal.SnocList
+
+import LLVM.AST hiding (function)
+import LLVM.AST.Global
+import LLVM.AST.Typed (typeOf)
+import qualified LLVM.AST.Type as T
+import qualified LLVM.AST.Constant as C
+import qualified LLVM.AST.Float as F
+
 import JIT
 import qualified Syntax as S
 
-toSig :: [String] -> [(AST.Type, AST.Name)]
-toSig = map (\x -> (double, AST.mkName x))
-
-codegenTop :: S.Expr -> LLVM ()
-codegenTop (S.Function name args body) = do
-  define double name fnargs bls
-  where
-    fnargs = toSig args
-    bls = createBlocks $ execCodegen $ do
-      entry <- addBlock entryBlockName
-      setBlock entry
-      forM args $ \a -> do
-        assign a (local (AST.mkName a))
-      cgen body >>= ret
-
-codegenTop (S.Extern name args) = do
-  external double name fnargs
-  where fnargs = toSig args
-
-codegenTop exp = do
-  define double "main" [] blks
-  where
-    blks = createBlocks $ execCodegen $ do
-      entry <- addBlock entryBlockName
-      setBlock entry
-      cgen exp >>= ret
 
 -------------------------------------------------------------------------------
--- Operations
+-- Helpers
 -------------------------------------------------------------------------------
 
-lt :: AST.Operand -> AST.Operand -> Codegen AST.Operand
-lt a b = do
-  test <- fcmp FP.ULT a b
-  uitofp double test
+local ::  Name -> Operand
+local = LocalReference T.double
 
+-- Converts a list of argument names to a list of double type arguments
+toSig :: [String] -> [(Type, ParameterName)]
+toSig = map (\x -> (T.double, ParameterName $ fromString x))
+
+-- Map of binary operators
+binops 
+  :: MonadIRBuilder m
+  => Map.Map String (Operand -> Operand -> m Operand)
 binops = Map.fromList [
       ("+", fadd)
     , ("-", fsub)
     , ("*", fmul)
     , ("/", fdiv)
-    , ("<", lt)
   ]
 
-cgen :: S.Expr -> Codegen AST.Operand
-cgen (S.UnaryOp op a) = do
-  cgen $ S.Call ("unary" ++ op) [a]
-cgen (S.BinaryOp "=" (S.Var var) val) = do
-  a <- getvar var
-  cval <- cgen val
-  store a cval
+-- Gets an operator from the operator list based on the name provided
+getop 
+  :: MonadIRBuilder m
+  => [Operand]         -- List of available operators
+  -> Name              -- Name of operator to find
+  -> m Operand         -- Found operator
+getop [] var = error $ "Local variable not in scope: " ++ show var
+getop (LocalReference ty nm:xs) var = 
+  if nm == var
+     then pure $ LocalReference ty nm
+     else getop xs var
+
+-- Function type that returns a double and only has double type for arguments
+doublef :: Int -> Type
+doublef n = FunctionType T.double args False
+  where
+    args = replicate n T.double
+
+-- Create a function pointer type with the given number of arguments
+fnPtr :: Int -> Name -> Operand
+fnPtr n = ConstantOperand . C.GlobalReference (T.ptr $ doublef n)
+
+-- Updates a module using the defintions from a ModuleBuilder
+updateModule 
+  :: Module            -- The module to update
+  -> ModuleBuilder a   -- The module builder to update with
+  -> Module            -- The updated module
+updateModule mod modb = mod 
+  { moduleDefinitions = execModuleBuilder emptyModuleBuilder modb
+  }
+
+-------------------------------------------------------------------------------
+-- Emitters
+-------------------------------------------------------------------------------
+
+-- Emits toplevel constructions in modules
+topgen 
+  :: MonadModuleBuilder m 
+  => S.Expr 
+  -> m Operand
+topgen (S.Function name args body) = do
+  function label argtys retty buildf
+  where
+    label  = mkName name
+    argtys = toSig args
+    retty  = T.double
+    buildf = \xs -> do
+      opgen xs body >>= ret
+
+topgen (S.Extern name args) = do
+  extern label argtys retty
+  where
+    label  = mkName name
+    argtys = [T.double | arg <- args]
+    retty  = T.double
+
+topgen exp = do
+  function "main" [] retty buildf
+  where
+    retty  = T.double
+    buildf = \xs -> do
+      opgen xs exp >>= ret
+
+-- Expression level generator that recurisvely walks the AST
+-- Maybe it should return functions that take operans? Idk
+opgen 
+  :: MonadIRBuilder m
+  => [Operand]    -- The list of in use operands
+  -> S.Expr           -- The to parse and convert into an operand
+  -> m Operand    -- The resulting operand
+opgen ops (S.UnaryOp op a) = do
+  opgen ops $ S.Call ("unary" ++ op) [a]
+
+opgen ops (S.BinaryOp "=" (S.Var var) val) = do
+  ptr <- getop ops (mkName var)
+  cval <- opgen ops val
+  store ptr 0 cval
   pure cval
-cgen (S.BinaryOp op a b) = do
+
+opgen ops (S.BinaryOp op a b) = do
   case Map.lookup op binops of
     Just f  -> do
-      ca <- cgen a
-      cb <- cgen b
+      ca <- opgen ops a
+      cb <- opgen ops b
       f ca cb
     Nothing -> error "No such operator"
-cgen (S.Var x) = getvar x
-cgen (S.Float n) = pure $ cons $ C.Float (F.Double n)
-cgen (S.Call fn args) = do
-  largs <- mapM cgen args
-  let len = length largs
-  call (externf len $ AST.mkName fn) largs
+
+opgen ops (S.Var x) = getop ops (mkName x)
+
+opgen _ (S.Float n) = pure $ ConstantOperand $ C.Float (F.Double n)
+
+opgen ops (S.Call fn args) = do
+  largs <- mapM (opgen ops) args
+  let len   = length largs
+  let rargs = [(arg, []) | arg <- largs]
+  call ptr rargs
+    where
+      len = length args
+      ptr = fnPtr len (mkName fn)
+      
 
 -------------------------------------------------------------------------------
 -- Compilation
 -------------------------------------------------------------------------------
 
-codegen :: AST.Module -> [S.Expr] -> IO AST.Module
+-- Generates string representation of the LLVM IR from the AST
+-- Uses JIT to generate LLVM IR with an optimization pass
+-- Uses topGen and opgen to generate from the AST
+codegen :: Module -> [S.Expr] -> IO Module
 codegen mod fns = do
     newast <- runJIT oldast
     pure newast
   where
-    modn    = mapM codegenTop fns
-    oldast  = runLLVM mod modn
+    modb    = mapM topgen fns
+    oldast  = updateModule mod modb
+
+
